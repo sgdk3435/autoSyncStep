c2b3d77767da65927a24ce1ec05d5dd9502a1e93
Hill <31231376+sgdk3435@users.noreply.github.com>
2025-10-17 09:55:57 +0800

    Refactor variable names in main.py for clarity

--- main.py
+++ main.py
@@ -94,8 +94,8 @@ def getBeijinTime():
                 msg_mi = "由于天气" + type + "，已设置降低步数,系数为" + str(K) + "。\n"
             else:
                 msg_mi = ""
-            for user_mi, passwd_mi in zip(user_list, passwd_list):
-                msg_mi += main(user_mi, passwd_mi, min_1, max_1)
+            for user_item, passwd_item in zip(user_list, passwd_list):
+                msg_mi += main(user_item, passwd_item, min_1, max_1)
                 # print(msg_mi)
     else:
         print("当前主人设置了0步数呢，本次不提交")
